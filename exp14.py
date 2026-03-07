a57f07a083302b2ffab2d562a8cad61876dd7263
ram <24jr1a05aq@gmail.com>
2026-03-07 14:05:26 +0530

    Add files via upload

--- exp14.py
+++ exp14.py
@@ -1,28 +1,9 @@
-feet=int(input('Enter length in feet '))
-while True:
-    print('1. Inches')
-    print('2. Yards')
-    print('3. Miles')
-    print('4. Millimeters')
-    print('5. Centimeters')
-    print('6. meters')
-    print('7. Kilometers')
-    print('8. Exit')
-    sid=int(input('Enter your choice '))
-    if sid==1:
-        con=feet*12
-    elif sid==2:
-        con=feet/3.0
-    elif sid==3:
-        cov=feet/5280.0
-    elif sid==4:
-        con=feet*304.8
-    elif sid==5:
-        con=feet*30.48
-    elif sid==6:
-        con=feet*0.3048
-    elif sid==7:
-        con=feet*0.0003048
-    elif sid==8:
-        break
-    print('The required result= ',res)
+a=["inches", "yards", "miles", "millimetres", "centi meters","meters","kilo meters"]
+b=[12,1/3,1/5280,304.8,30.48,0.3048,0.0003048]
+n=int(input("Enter a numer:"))
+k=int(input("Enter a value to read\n 1.inches\n 2.yards\n 3.miles\n 4.millimetres\n 5.centi meters\n 6.meters\n 7.kilo meters\n"))
+while k<8:
+    n=int(input("Enter a valid option:"))
+    print(n,"Feet:",n*b[k-1],a[k-1])
+    break;
+   
---
+++ exp15.py
@@ -0,0 +1,13 @@
+sum=0
+def sum_digits(n):
+    global sum;
+    for i in n:
+      sum=sum+int(i)
+n=input("Enter number")
+sum_digits(n)
+print(sum)
+    
+    
+    
+    
+               
